8e0e480a7f15bb3b9bbd6ab4fc711664c767491e
Timur <timka_issaev@mail.ru>
2021-03-21 19:21:20 +0300

    added the main variables

--- js/common.js
+++ js/common.js
@@ -1,2 +1,18 @@
-var canvas = document.getElementById("canvas");
-var canvas_content = cvs.getContext("2d");
+var canvas = document.getElementById("canvas"),
+             canvas_content = canvas.getContext("2d"),
+             bird = new Image(),
+             backgrond = new Image(),
+             foreground = new Image(),
+             pipeUp = new Image(),
+             pipeBottom = new Image(),
+             fly = new Audio(),
+             score_audio = new Audio();
+bird.src = "images/bird.png";
+backgrond.src = "images/bg.png";
+foreground.src = "images/fg.png";
+pipeUp.src = "images/pipeUp.png";
+pipeBottom.src = "images/pipeBottom.png";
+
+fly.src = 'audio/fly.mp3';
+score_audio.src = 'audio/score.mp3';
+
